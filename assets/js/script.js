2e3c16a352108c7de4bb513ee8754697aff8d1ab
qtian13 <qiushuang.tian@gmail.com>
2021-06-26 09:28:42 -0700

    change the outlook   deal with timer and interval

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,66 @@ var wordArray;
 var indexOfCurrentWord;
 var numWrongChar;
 var validInput = "abcdefghijklmnopqrstuvwxyz-";
-var secondsLeft = 60;
+var timeTotal = 20;
+var secondsLeft = timeTotal;
 var wordBox = document.querySelector(".word-box");
 var elementToRemoveKeypressListner;
 var numWins = 0;
 var numLosses = 0;
 var charSelected = null;
+var myTimer;
+var myInterval;
 
 function startGame() {
-    wordArray = ["elephant", "cat", "rabbit", "bee", "dog"];
+    wordArray = ["dog", "cat", "elephant"];
     startTimer();
     startGuess();
 }
 
 function reset() {
+    clearTimeout(myTimer);
+    clearTimeout(myInterval);
     indexOfCurrentWord = 0;
+    wordBox.innerHTML = "";
     document.querySelector("#wins").textContent = "Wins: 0";
     document.querySelector("#losses").textContent = "Losses: 0";
-    document.querySelector("#time-left").textContent = "TIME LEFT: " + secondsLeft + "s";
-
+    secondsLeft = timeTotal;
+    document.querySelector("#time-left").textContent = "Time left: " + secondsLeft + "s";
 }
 
 function startTimer(){
-    var myInterval = setInterval(function() {
+    myInterval = setInterval(function() {
         secondsLeft--;
-        document.querySelector("#time-left").textContent = "TIME LEFT: " + secondsLeft + "s";
+        document.querySelector("#time-left").textContent = "Time left: " + secondsLeft + "s";
     }, 1000 * 1);
-    setTimeout(function () {
+    myTimer = setTimeout(function () {
         clearInterval(myInterval);
-        if (indexOfCurrentWord != wordArray.length) {
-            numLosses++;
-            document.querySelector("#losses").textContent = "Losses: " + numLosses;
+        displayLoss();
+    }, 1000 * timeTotal);
+}
+
+function displayLoss() {
+    numLosses++;
+    document.querySelector("#losses").textContent = "Losses: " + numLosses;
+    setTimeout(function() {
+        var tryAgain = alert("Time is up! Want to try again?");
+        reset();
+        if (tryAgain) {
+            startGame();
         }
-    }, 1000 * 60);
+    }, 1);
 }
 
 function startGuess() {
     var currentWord = wordArray[indexOfCurrentWord];
     numWrongChar = currentWord.length;
-    wordBox.innerHTML = "";
     for(var i = 0; i < currentWord.length; i++) {
         var element = document.createElement("div");
         element.setAttribute("data-expected", currentWord[i]);
         element.setAttribute("data-guessed", "false");
         element.setAttribute("class", "character");
         element.setAttribute("id", "character" + i);
-        element.setAttribute("style", "font-size: 30px; border-bottom: 2px solid black; width: 40px; height: 40px; display: flex; justify-content: center; margin: 5px;");
+        element.setAttribute("style", "border-bottom: 2px solid black; width: 40px; height: 40px; display: flex; justify-content: center; margin: 5px;");
         wordBox.appendChild(element);
     }
 }
@@ -75,16 +89,25 @@ function checkResult(char) {
         if (char == charSelected.dataset.expected) {
             charSelected.dataset.guessed = "true";
             charSelected.textContent = char;
+            console.log(charSelected.textContent);
+            console.log(char);
             charSelected.style["border-bottom"] = "none";
             numWrongChar--;
             if (numWrongChar == 0) {
                 indexOfCurrentWord++;
-                if (indexOfCurrentWord != wordArray.length) {
-                    startGuess();
-                } else {
-                    numWins++;
-                    document.querySelector("#wins").textContent = "Wins: " + numWins;
-                }
+                setTimeout(function() {
+                    if (indexOfCurrentWord != wordArray.length) {
+                        wordBox.innerHTML = "";
+                        startGuess();
+                    } else {
+                        numWins++;
+                        document.querySelector("#wins").textContent = "Wins: " + numWins;
+                        setTimeout(function () {
+                            alert("Congratulations! You win!");
+                            reset();
+                        }, 1000 * 0.2);
+                    }
+                }, 1);
             }
         }   
     }
